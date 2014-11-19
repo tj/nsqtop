784f6e6974f53a635521475b2111f30deb44813e
TJ Holowaychuk <tj@vision-media.ca>
2014-11-18 18:06:14 -0800

    fmt

--- pkg/nsqd/stats.go
+++ pkg/nsqd/stats.go
@@ -2,39 +2,39 @@ package nsqd
 
 // stats with envelope.
 type stats struct {
-  Data *Stats
+	Data *Stats
 }
 
 // Stats.
 type Stats struct {
-  Version string   `json:"version"`
-  Health  string   `json:"health"`
-  Topics  []*Topic `json:"topics"`
+	Version string   `json:"version"`
+	Health  string   `json:"health"`
+	Topics  []*Topic `json:"topics"`
 }
 
 // Topic stats.
 type Topic struct {
-  Name          string     `json:"topic_name"`
-  InFlightCount int64      `json:"in_flight_count"`
-  DeferredCount int64      `json:"deferred_count"`
-  MessageCount  int64      `json:"message_count"`
-  RequeueCount  int64      `json:"requeue_count"`
-  TimeoutCount  int64      `json:"timeout_count"`
-  BackendDepth  int64      `json:"backend_depth"`
-  Depth         int64      `json:"depth"`
-  Paused        bool       `json:"paused"`
-  Channels      []*Channel `json:"channels"`
+	Name          string     `json:"topic_name"`
+	InFlightCount int64      `json:"in_flight_count"`
+	DeferredCount int64      `json:"deferred_count"`
+	MessageCount  int64      `json:"message_count"`
+	RequeueCount  int64      `json:"requeue_count"`
+	TimeoutCount  int64      `json:"timeout_count"`
+	BackendDepth  int64      `json:"backend_depth"`
+	Depth         int64      `json:"depth"`
+	Paused        bool       `json:"paused"`
+	Channels      []*Channel `json:"channels"`
 }
 
 // Channel stats.
 type Channel struct {
-  Name          string `json:"channel_name"`
-  InFlightCount int64  `json:"in_flight_count"`
-  DeferredCount int64  `json:"deferred_count"`
-  MessageCount  int64  `json:"message_count"`
-  RequeueCount  int64  `json:"requeue_count"`
-  TimeoutCount  int64  `json:"timeout_count"`
-  BackendDepth  int64  `json:"backend_depth"`
-  Depth         int64  `json:"depth"`
-  Paused        bool   `json:"paused"`
+	Name          string `json:"channel_name"`
+	InFlightCount int64  `json:"in_flight_count"`
+	DeferredCount int64  `json:"deferred_count"`
+	MessageCount  int64  `json:"message_count"`
+	RequeueCount  int64  `json:"requeue_count"`
+	TimeoutCount  int64  `json:"timeout_count"`
+	BackendDepth  int64  `json:"backend_depth"`
+	Depth         int64  `json:"depth"`
+	Paused        bool   `json:"paused"`
 }
